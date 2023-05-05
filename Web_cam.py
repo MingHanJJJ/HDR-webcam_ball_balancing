093f433a7f51dc0b0fe0017ad87eaf58ddd13a42
Ming-Han Hsieh <jeffhsieh0987@gmail.com>
2023-05-05 15:42:46 -0400

    5/5

--- Web_cam.py
+++ Web_cam.py
@@ -11,7 +11,7 @@ class WebCam:
 
     def video_capture(self):
         # Webcamera no 0 is used to capture the frames
-        return cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        return cv2.VideoCapture(1, cv2.CAP_DSHOW)
     
     def dectect_boundary(self, pixels, buffer_pixels):
         x_max = y_max = 0
@@ -44,7 +44,11 @@ class WebCam:
         return True
 
     def start_webcam(self):
-        
+        dist = lambda x1,y1,x2,y2: (x1-x2)**2+(y1-y2)**2
+        prevCircle = None 
+        chosen = [0,0]
+        serial = serial_port.Serial_port()
+
         # This drives the program into an infinite loop.
         while(1):        
             # Captures the live stream frame-by-frame
@@ -58,15 +62,49 @@ class WebCam:
             rects = []
             max_w = 0
             max_h = 0
+            rect_x = 0
+            rect_y = 0
             for contour in contours:
                 x,y,w,h = cv2.boundingRect(contour)
-                max_w = max(max_w, w)
-                max_h = max(max_h, h)
+                if(max_w < w): 
+                    max_w = max(max_w, w)
+                    max_h = max(max_h, h)
+                    rect_x = x
+                    rect_y = y
                 rects.append((x,y,w,h))
-            print(len(rects))
-            print("max w: %s, max h: %s" % (max_w, max_h))
 
-            cv2.imshow('frame',frame)             
+            # print("x:%s  y:%s" % (rect_x, rect_y))
+            # print("max w: %s, max h: %s" % (max_w, max_h))
+            frame = cv2.circle(frame, (rect_x, rect_y), radius=10, color=(0, 0, 255), thickness=-1)
+            frame = cv2.circle(frame, (rect_x + max_w, rect_y), radius=10, color=(0, 0, 255), thickness=-1)
+            frame = cv2.circle(frame, (rect_x + max_w, rect_y + max_h), radius=10, color=(0, 0, 255), thickness=-1)
+            frame = cv2.circle(frame, (rect_x, rect_y + max_h), radius=10, color=(0, 0, 255), thickness=-1)
+
+            grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            blurFrame = cv2.GaussianBlur(grayFrame, (17, 17), 0)
+            circles = cv2.HoughCircles(blurFrame, cv2.HOUGH_GRADIENT, 0.5, 100, param1=100, param2=30, minRadius=10, maxRadius=50) 
+            if circles is not None:
+                circles = np.uint16(np.around(circles))
+                chosen = None
+                for i in circles[0, :]:
+                    if chosen is None:
+                        chosen = i
+                    if prevCircle is not None:
+                        if dist(chosen[0],chosen[1],prevCircle[0],prevCircle[1]) <= dist(i[0], i[1], prevCircle[0], prevCircle[1]):
+                            chosen = i
+                #chosen[0], chosen[1] are x, y coordinations; chosen[2] represents the radius of circle that cv.circle() is drawing
+                cv2.circle(frame, (chosen[0], chosen[1]), 1, (0, 100, 100), 3)
+                cv2.circle(frame, (chosen[0], chosen[1]), chosen[2], (255, 0, 255), 3)
+
+                #print(f"Center of the circle: ({chosen[0]}, {chosen[1]})")
+                prevCircle = chosen
+
+            
+
+            ##############
+            cv2.imshow('frame',frame)   
+            
+            serial.send_poition((chosen[0].item()), (chosen[1].item()))          
         
             # This displays the frame, mask 
             # and res which we created in 3 separate windows.
@@ -74,6 +112,7 @@ class WebCam:
             if k == 27:
                 break
 
+            
             
         # Destroys all of the HighGUI windows.
         cv2.destroyAllWindows()
--- serial_port.py
+++ serial_port.py
@@ -1,25 +1,41 @@
 from pickletools import int4
 from secrets import token_bytes
+import time
 import serial
 
 class Serial_port:
     port = None
     def __init__(self) -> None:
-        self.port = serial.Serial(port="COM1", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+        self.port = serial.Serial(port="COM4", baudrate=9600, timeout=1)
+        
     
     def send_boundary(self):
         pass
 
     def send_poition(self, x:int, y:int):
-        x_byte = x.to_bytes(1, 'little')
-        y_byte = y.to_bytes(1, 'little')
-        
+        x_byte = x.to_bytes(2, 'big')
+        y_byte = y.to_bytes(2, 'big')
         """
         the write function have the start bit (which is always 0), the data bits (in this case, one or two bytes representing the integer)
         , and the stop bit (which is always 1) to create a complete data  
         """
-        self.port.write(x)
-        self.port.write(y)
+        
+        self.port.write(x_byte)
+        self.port.write(y_byte)
 
     def close(self):
         self.port.close()
+
+if __name__ == "__main__":
+    sr = Serial_port()
+    while(1):
+        # sr.send_poition(612,535)
+        crc = 32678
+        sr.port.write(crc.to_bytes(2, 'big'))
+        print("send")
+        time.sleep(1) 
+
+
+  
+
+  
